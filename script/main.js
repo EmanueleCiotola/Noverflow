a9a7c134d54f9020ceb8b2d9425df96a0468eb7b
Devma <66367222+EmanueleCiotola@users.noreply.github.com>
2024-10-03 01:13:53 +0200

    Noverflow 2.0 HomePage

---
+++ script/main.js
@@ -0,0 +1,16 @@
+// gestione items osservati
+var elementiDaOsservare = document.querySelectorAll('.osservato');
+var callback = function(items) {
+    items.forEach((item) => {
+        var position = item.target.getBoundingClientRect();
+        if (item.isIntersecting) {
+            item.target.classList.add("mostrato");
+        } else if (position.top > 0) { // rimuove la classe mostrato solo se l'item si trova nella parte non visibile superiore dello schermo
+            item.target.classList.remove("mostrato");
+        }
+    });
+}
+var observer = new IntersectionObserver(callback, {threshold: 0});
+elementiDaOsservare.forEach((element) => {
+    observer.observe(element);
+});
